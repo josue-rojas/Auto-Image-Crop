514a6ecbfc681de157da6a4420c1fed0f8460c66
josue rojas <josuerojas.rojas@gmail.com>
2017-08-15 15:33:53 -0400

    init github version

---
+++ clean.py
@@ -0,0 +1,8 @@
+import os
+with open('.gitignore','r') as gitignore:
+    newIgnore = gitignore.readlines()
+newIgnore[2] = 'images/*'
+with open('.gitignore','w') as gitignore:
+    gitignore.writelines(newIgnore)
+    gitignore.close()
+os.system('git rm -r --cached .; git add .; git commit -m "deleted images"; git push')
--- localhost.py
+++
@@ -1,9 +0,0 @@
-import SimpleHTTPServer
-import SocketServer
-
-PORT = 8080
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-print "listening in port: ", PORT
-httpd.serve_forever()
